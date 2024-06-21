a8c3f5e7501e72ebde410be5e17eb274c24fdafd
BishalMrzn <112357355+BishalMrzn@users.noreply.github.com>
2024-06-21 19:49:09 +0545

    Add files via upload

---
+++ Practice_set1/practise.js
@@ -0,0 +1,8 @@
+const dict={
+    "work":"effort made in order to achieve or make something",
+    "shrivel":"to become dry, smaller, and covered with lines as if by crushing or folding, or to make something do this",
+    "advice":"suggestions to a person about what he or she should do",
+    "vacation":"a holiday",
+    "be meant to":"to be required or supposed"
+}
+console.log(dict)
